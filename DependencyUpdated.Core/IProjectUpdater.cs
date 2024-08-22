d601288763c6f827697b86c03204bb30165f2de9
Mateusz Pniewski <mateusz.pniewski@gmail.com>
2024-08-22 10:42:15 +0200

    fix

--- DependencyUpdated.Core/IProjectUpdater.cs
+++ DependencyUpdated.Core/IProjectUpdater.cs
@@ -5,6 +5,8 @@ namespace DependencyUpdated.Core;
 public interface IProjectUpdater
 { 
     Task<ICollection<DependencyDetails>> ExtractAllPackagesThatNeedToBeUpdated(string fullPath, Project projectConfiguration);
+    
     IEnumerable<string> GetAllProjectFiles(string searchPath);
+    
     IReadOnlyCollection<UpdateResult> HandleProjectUpdate(string fullPath, ICollection<DependencyDetails> dependenciesToUpdate);
 }
