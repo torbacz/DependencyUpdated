1d845a4b21e0edcd3c4770e22f06fa8c75f146c6
Marcin Torba <torbamarcin@gmail.com>
2024-10-15 21:32:22 +0200

    Fixes

--- src/DependencyUpdated.Projects.Npm/NpmUpdater.cs
+++ src/DependencyUpdated.Projects.Npm/NpmUpdater.cs
@@ -74,6 +74,53 @@ internal sealed class NpmUpdater : IProjectUpdater
         return updates;
     }
     
+    public async Task<ICollection<DependencyDetails>> ExtractAllPackages(IReadOnlyCollection<string> fullPath)
+    {
+        return await Task.FromResult(fullPath.SelectMany(ParseProject).ToHashSet());
+    }
+    
+    public async Task<IReadOnlyCollection<DependencyDetails>> GetVersions(DependencyDetails package,
+        Project projectConfiguration)
+    {
+        #pragma warning disable S1075
+        var npmApi = RestService.For<INpmApi>("https://registry.npmjs.org");
+        var data = await npmApi.GetPackageData(package.Name);
+        return data.Versions.Where(x => IsValidVersion(x.Value.Version))
+            .Select(x => new DependencyDetails(x.Key, new Version(x.Value.Version))).ToList();
+    }
+
+    private static HashSet<DependencyDetails> ParseProject(string path)
+    {
+        var json = File.ReadAllText(path);
+        var package = JsonSerializer.Deserialize<PackageRoot>(json, JsonSerializerOptions);
+        
+        if (package is null)
+        {
+            return new HashSet<DependencyDetails>();
+        }
+
+        var dependencies = package.Dependencies
+            .Select(d => new DependencyDetails(d.Key, ParseVersion(d.Value))).ToList();
+        var devDependencies = package.DevDependencies
+            .Select(d => new DependencyDetails(d.Key, ParseVersion(d.Value))).ToList();
+        return dependencies.Concat(devDependencies).ToHashSet();
+    }
+
+    private static Version ParseVersion(string data)
+    {
+        return new Version(data.TrimStart('^').TrimStart('~'));
+    }
+
+    private static bool IsValidVersion(string data)
+    {
+        if (data.Contains('-'))
+        {
+            return false;
+        }
+
+        return true;
+    }
+    
     private bool IsNpmInstalled()
     {
         try
@@ -101,51 +148,5 @@ internal sealed class NpmUpdater : IProjectUpdater
         {
             return false;
         }
-    }
-
-    public async Task<ICollection<DependencyDetails>> ExtractAllPackages(IReadOnlyCollection<string> fullPath)
-    {
-        return await Task.FromResult(fullPath.SelectMany(ParseProject).ToHashSet());
-    }
-
-    private static HashSet<DependencyDetails> ParseProject(string path)
-    {
-        var json = File.ReadAllText(path);
-        var package = JsonSerializer.Deserialize<PackageRoot>(json, JsonSerializerOptions);
-        
-        if (package is null)
-        {
-            return new HashSet<DependencyDetails>();
-        }
-
-        var dependencies = package.Dependencies
-            .Select(d => new DependencyDetails(d.Key, ParseVersion(d.Value))).ToList();
-        var devDependencies = package.DevDependencies
-            .Select(d => new DependencyDetails(d.Key, ParseVersion(d.Value))).ToList();
-        return dependencies.Concat(devDependencies).ToHashSet();
-    }
-
-    private static Version ParseVersion(string data)
-    {
-        return new Version(data.TrimStart('^').TrimStart('~'));
-    }
-
-    public async Task<IReadOnlyCollection<DependencyDetails>> GetVersions(DependencyDetails package,
-        Project projectConfiguration)
-    {
-        var npmApi = RestService.For<INpmApi>("https://registry.npmjs.org");
-        var data = await npmApi.GetPackageData(package.Name);
-        return data.Versions.Where(x => IsValidVersion(x.Value.Version))
-            .Select(x => new DependencyDetails(x.Key, new Version(x.Value.Version))).ToList();
-    }
-
-    private static bool IsValidVersion(string data)
-    {
-        if (data.Contains('-'))
-        {
-            return false;
-        }
-
-        return true;
     }
 }
